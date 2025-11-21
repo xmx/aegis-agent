9782fbe960d83502688b79c60fb0dfd182ae8e97
xmx <no@example.com>
2025-11-21 21:06:27 +0800

    简化机器码

--- clientd/machine.go
+++ clientd/machine.go
@@ -4,9 +4,7 @@ import (
 	"crypto/rand"
 	"crypto/sha1"
 	"encoding/hex"
-	"net"
 	"os"
-	"slices"
 	"strings"
 
 	"github.com/xmx/aegis-agent/machine"
@@ -61,104 +59,12 @@ func (m *machineIdent) store(id string) {
 
 func (m *machineIdent) generate() string {
 	mid, _ := machine.ID()
-	hostname, _ := os.Hostname()
-	card := m.networks()
-	str := strings.Join([]string{mid, hostname, card}, ",")
-	sum := sha1.Sum([]byte(str))
+	if mid == "" {
+		buf := make([]byte, 20)
+		_, _ = rand.Read(buf)
+		mid = hex.EncodeToString(buf)
+	}
+	sum := sha1.Sum([]byte(mid))
 
 	return hex.EncodeToString(sum[:])
-}
-
-func (m *machineIdent) networks() string {
-	faces, _ := net.Interfaces()
-	cards := make(nics, 0, len(faces))
-	for _, face := range faces {
-		// 跳过换回网卡和未启用的网卡
-		if face.Flags&net.FlagUp == 0 ||
-			face.Flags&net.FlagLoopback != 0 ||
-			face.Flags&net.FlagPointToPoint != 0 {
-			continue
-		}
-
-		emptyMAC := true
-		for _, b := range face.HardwareAddr {
-			if b != 0 {
-				emptyMAC = false
-				break
-			}
-		}
-		if emptyMAC { // MAC 地址为空，一般都是虚拟网卡。
-			continue
-		}
-
-		var ips []string
-		addrs, _ := face.Addrs()
-		for _, addr := range addrs {
-			var ip net.IP
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
-			default:
-				continue
-			}
-
-			// 过滤无效地址
-			if ip == nil ||
-				ip.IsLoopback() ||
-				ip.IsMulticast() ||
-				ip.IsUnspecified() {
-				continue
-			}
-
-			if ip4 := ip.To4(); ip4 != nil {
-				ips = append(ips, ip.String())
-			} /* else if ip.To16() != nil {
-				// 排除 IPv6 链路本地地址（fe80::/10），如不需要可移除此条件
-				if ip.IsLinkLocalUnicast() {
-					continue
-				}
-				ips = append(ips, ip.String())
-			}*/
-		}
-		if len(ips) != 0 {
-			cards = append(cards, &nic{
-				MAC:   face.HardwareAddr.String(),
-				Inets: ips,
-			})
-		}
-	}
-	cards.sort()
-
-	return cards.join()
-}
-
-type nic struct {
-	MAC   string
-	Inets []string
-}
-
-type nics []*nic
-
-func (ns nics) sort() {
-	slices.SortFunc(ns, func(a, b *nic) int {
-		return strings.Compare(a.MAC, b.MAC)
-	})
-	for _, n := range ns {
-		slices.Sort(n.Inets)
-	}
-}
-
-func (ns nics) join() string {
-	strs := make([]string, 0, len(ns))
-	for _, n := range ns {
-		ele := make([]string, 0, len(n.Inets)+1)
-		ele = append(ele, n.MAC)
-		ele = append(ele, n.Inets...)
-		line := strings.Join(ele, ",")
-		strs = append(strs, line)
-	}
-
-	return strings.Join(strs, ",")
 }
